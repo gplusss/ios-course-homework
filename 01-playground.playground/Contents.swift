02d7e833875cfe142f9379a19143d73ada45d7fa
Vladimir Saprykin <gplusss@gmail.com>
2016-10-16 23:47:43 +0300

    edit_2

--- 01-playground.playground/Contents.swift
+++ 01-playground.playground/Contents.swift
@@ -5,31 +5,46 @@ import UIKit
 class AppDiary {
     
     var date = Date()
-    var name: String?
-    var text: String?
-    var tag: String?
+    var name: String
+    var text: String
+    var tags: [String]
     
-    init(date: Date, name: String, text: String, tag: String) {
+    init(date: Date, name: String, text: String, tags: [String]) {
         self.date = date
         self.name = name
         self.text = text
-        self.tag = tag
+        self.tags = tags
     }
 
     func fullDescrition() -> String {
+        var result = date.description
+
+        if (name.isEmpty) {
+            result += "\n" + name
+        }
+        
+        if (tags.count) > 0 {
+            result += "\n"
+            for i in tags {
+                result += "[" + i + "]"
+            }
+        }
+        if (text.isEmpty) {
+            result += "\n" + text
+        }
     
-        return "\(date)\n\(name)\n\(text)\n[\(tag)]\n"
-    
+        //return result
+        return String(describing: date) + "\n" + name + "\n" + text + "\n" + String(describing: tags)
     }
+    
 }
 
-let first = AppDiary(date: Date(), name: "Завтрак", text: "Пью кофе, холодно и грустно, потому что осень", tag: "весело")
-let second = AppDiary(date: Date(), name: "Обед", text: "Пью кофе, холодно и грустно, потому что осень", tag: "грустно")
+let first = AppDiary(date: Date(), name: "Завтрак", text: "Пью кофе, холодно и грустно, потому что не сразу получилось", tags: ["весело",  "грустно"])
+let second = AppDiary(date: Date(), name: "Обед", text: "Пью кофе, весело и радостно, потому что Swift", tags: ["грустно"])
 
 
 print(first.fullDescrition())
 print(second.fullDescrition())
-
 
 
 
