cda495f772fb800f5ac27c3cc8391ea65b3c5da4
Vladimir Saprykin <gplusss@gmail.com>
2016-10-16 19:24:09 +0300

    edit_1

--- 01-playground.playground/Contents.swift
+++ 01-playground.playground/Contents.swift
@@ -4,32 +4,33 @@ import UIKit
 
 class AppDiary {
     
-    var date: String
-    var name: String
-    var text: String
-    var tag: String
+    var date = Date()
+    var name: String?
+    var text: String?
+    var tag: String?
     
-    
-    init(date: String, name: String, text: String, tag: String) {
+    init(date: Date, name: String, text: String, tag: String) {
         self.date = date
         self.name = name
         self.text = text
         self.tag = tag
     }
-    func fullDEscrition() -> String {
-        return "\(date)\n\(name)\n\(text)\n[\(tag)]\n"
-    }
 
+    func fullDescrition() -> String {
+    
+        return "\(date)\n\(name)\n\(text)\n[\(tag)]\n"
+    
+    }
 }
 
-
-let first = AppDiary(date: "12 мая 2016 г.", name: "Завтрак", text: "Яичница", tag: "весело")
-let second = AppDiary(date: "13 июня 2015 г.", name: "Обед", text: "Суп", tag: "грустно")
-
-print(first.fullDEscrition())
-print(second.fullDEscrition())
-
-let array = [first, second]
+let first = AppDiary(date: Date(), name: "Завтрак", text: "Пью кофе, холодно и грустно, потому что осень", tag: "весело")
+let second = AppDiary(date: Date(), name: "Обед", text: "Пью кофе, холодно и грустно, потому что осень", tag: "грустно")
 
 
- 
+print(first.fullDescrition())
+print(second.fullDescrition())
+
+
+
+
+
